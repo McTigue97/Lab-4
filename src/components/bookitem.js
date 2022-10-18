f09f20def1a31600f668154a4e6cb174c85a6d74
McTigue97 <78153480+McTigue97@users.noreply.github.com>
2022-10-18 12:21:32 +0100

    Update bookitem.js

--- src/components/bookitem.js
+++ src/components/bookitem.js
@@ -11,12 +11,14 @@ export class BookItem extends React.Component{
 
                 {/* Card Holder to tidy up the page */}
                 <Card>
+                    {/* Each book item title will display */}
                     <Card.Header>{this.props.book.title}</Card.Header>
                     <Card.Body>
                         <blockquote className="blockquote mb-0">
+                        {/* A book item thumbnail will display */}
                        <img src={this.props.book.thumbnailUrl} width="200" height="200"></img>
                         <footer>
-                            {this.props.book.authors[0]}
+                            {this.props.book.authors[0]} {/* Each book item authors will display */}
                         </footer>
                         </blockquote>
                     </Card.Body>
